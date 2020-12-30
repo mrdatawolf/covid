4a21406703b35929c0061d1037f25c11477c47d2
Patrick Moon <patrickmoon@outlook.com>
2020-12-29 18:25:22 -0800

    cleaning up the title description

--- resources/views/livewire/charts/number-state-uses.blade.php
+++ resources/views/livewire/charts/number-state-uses.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <h2 title="The State counts our numbers using the last tuesday ({{ $laggedDate  }}) from the To date. This is based on the rules they have posted on determining current county numbers. However their numbers seldom match the rules given."><a href="https://covid19.ca.gov/safer-economy/"><img src="https://covid19.ca.gov/img/icons/favicon-32x32.png"></a> Expected State count: {{ $laggedCount }}</h2>
+    <h2 title="The State counts our numbers with a 7 day lag ({{ $laggedDate  }}) from the To date. This is based on the rules they have posted on determining current county numbers. However their numbers seldom match the rules given."><a href="https://covid19.ca.gov/safer-economy/"><img src="https://covid19.ca.gov/img/icons/favicon-32x32.png"></a> Expected State count: {{ $laggedCount }}</h2>
 </div>
--- resources/views/livewire/charts/tuesday-average.blade.php
+++ resources/views/livewire/charts/tuesday-average.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <h2 title="The State counts our numbers using the last tuesday ({{ $tuesdayDate  }}) from the To date. This is based on the rules they have posted on determining current county numbers. However their numbers seldom match the rules given."><a href="https://humboldtgov.org/2707/COVID-Test-Results"><img src="https://humboldtgov.org/images/favicon.ico"></a>Expected County count: {{ $tuesdayCount }}</h2>
+    <h2 title="The County counts our numbers using the last tuesday ({{ $tuesdayDate  }}) from the To date. This is based on the rules they have posted on determining current county numbers. However their numbers seldom match the rules given."><a href="https://humboldtgov.org/2707/COVID-Test-Results"><img src="https://humboldtgov.org/images/favicon.ico"></a>Expected County count: {{ $tuesdayCount }}</h2>
 </div>
