996a78dc74361c3592b448d8f12a9894fc96efe6
Patrick Moon <patrickmoon@outlook.com>
2020-12-31 13:23:35 -0800

    fixes the main menu on small displays
    cleans up how the rows display.
    Also aligns naming scheme

---
+++ resources/views/livewire/charts/number-county-uses.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <label title="The County reports our numbers using the last tuesday ({{ $countyDate  }}) from the To date. However their numbers seldom match the rules given." for="county-count" class="text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4">Expected <a href="https://humboldtgov.org/2707/COVID-Test-Results"><img src="https://humboldtgov.org/images/favicon.ico" style="display: inline"></a> count:</label>
+    <input type="text" wire:model="countyCount" id="county-count" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-grey focus:border-purple-500" readonly>
+</div>
--- resources/views/livewire/charts/seven-day-average.blade.php
+++ resources/views/livewire/charts/seven-day-average.blade.php
@@ -1,3 +1,4 @@
 <div>
-    <span title="This is always a average of the 7 days going back from and starting with on {{ $sevenDaysEndDate }}">7 day average : {{ $sevenDaysAverage }} </span>
+    <label title="This is an average of the 7 days going back from and starting with {{ $sevenDaysEndDate }}" for="seven-day-average" class="text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4">7 day average:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+    <input type="text" wire:model="sevenDaysAverage" id="seven-day-average" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-grey focus:border-purple-500" readonly>
 </div>
