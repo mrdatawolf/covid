99314a0d440e631ceb934518a9d9ec5c8304dcf6
Patrick Moon <patrickmoon@outlook.com>
2020-12-30 23:58:51 -0800

    fixes the county number to go by per 100k numbers
    Also adds the LCO link and attempts to make it smaller.
    Also changes the number to round to one decimal like the county dashboard view.

--- app/Http/Livewire/Charts/SevenDayAverage.php
+++ app/Http/Livewire/Charts/SevenDayAverage.php
@@ -46,12 +46,13 @@ class SevenDayAverage extends Component
      */
     private function getAverageCountOverDays($daysBack = 7)
     {
+        $perHunderedThousandModifier = 1.36;
         $rawToEnd    = Carbon::createFromFormat('Y-m-d H:i:s', $this->sevenDaysEndDate.' 23:59:59', 'America/Los_Angeles');
         $newestDate  = $rawToEnd->copy()->timezone('UTC');
         $oldiestDate = $rawToEnd->copy()->timezone('UTC')->subDays($daysBack)->startOfDay();
         $totalCounts = CountDaily::orderBy('created_at')
                                  ->where('created_at', '>=', $oldiestDate)
                                  ->where('created_at', '<=', $newestDate);
-        return ceil($totalCounts->sum('count') / $daysBack);
+        return round(($totalCounts->sum('count') / $daysBack)/$perHunderedThousandModifier, 1);
     }
 }
