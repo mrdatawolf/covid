c37eda2303e0a11b68c9d3c1b164742af571d254
Patrick Moon <patrickmoon@outlook.com>
2020-12-30 23:47:35 -0800

    fixing how I did the lag days.
    Also making it more flexible.
    Also changing the notes on the state page

--- app/Http/Livewire/Charts/NumberStateUses.php
+++ app/Http/Livewire/Charts/NumberStateUses.php
@@ -10,6 +10,7 @@ class NumberStateUses extends Component
 {
     public  $laggedCount;
     public  $laggedDate;
+    public  $stateLagDays;
     public  $secondaryLagDays;
 
     protected $listeners = [
@@ -18,7 +19,8 @@ class NumberStateUses extends Component
 
     public function mount() {
         $date = Carbon::now();
-        $this->findAWeekAgo($date);
+        $this->findLag($date);
+        $this->stateLagDays = 7;
         $this->secondaryLagDays = 2;
     }
 
@@ -32,32 +34,32 @@ class NumberStateUses extends Component
 
     public function updatedToDate($toDate)
     {
-        $this->findAWeekAgo($toDate);
+        $this->findLag($toDate);
     }
 
 
-    private function findAWeekAgo($toDate) {
+    private function findLag($toDate) {
+        $totalDaysBack = $this->stateLagDays+$this->secondaryLagDays;
         $rawToDate = (is_object($toDate)) ? $toDate :
-            Carbon::createFromFormat('Y-m-d', $toDate)->setTimezone('America/Los_Angeles')->endOfDay()->setTimezone('America/Los_Angeles');
-        $tuesday    = $rawToDate->copy()->subDays(7);
-        $this->laggedDate = $tuesday->toDateString();
+            Carbon::createFromFormat('Y-m-d', $toDate)->setTimezone('America/Los_Angeles')->endOfDay();
+        $laggedDate    = $rawToDate->copy()->subDays($totalDaysBack);
+        $this->laggedDate = $laggedDate->toDateString();
     }
 
 
-    private function getAverageCountForLaggedDay($daysBack = 7)
+    private function getAverageCountForLaggedDay()
     {
-        $totalLagDays = $this->secondaryLagDays + $daysBack;
         $perHunderedThousandModifier = 1.36;
         $currentTime = Carbon::now()->toTimeString();
         $rawToEnd    = Carbon::createFromFormat('Y-m-d H:i:s', $this->laggedDate.' '.$currentTime)
                              ->setTimezone('America/Los_Angeles');
         $newestDate  = $rawToEnd->copy()->endOfDay();
-        $oldiestDate = $rawToEnd->copy()->subDays($daysBack)->startOfDay();
+        $oldiestDate = $rawToEnd->copy()->subDays($this->stateLagDays)->startOfDay();
         $totalCounts = CountDaily::orderBy('created_at')
                                  ->where('created_at', '>=', $oldiestDate)
                                  ->where('created_at', '<=', $newestDate);
         $summedCount = $totalCounts->sum('count');
-        $this->laggedCount = (empty($summedCount) || empty($daysBack) || empty($perHunderedThousandModifier)) ? 0
-        : round(($summedCount / $totalLagDays)/$perHunderedThousandModifier, 2);
+        $this->laggedCount = (empty($summedCount) || empty($this->stateLagDays) || empty($perHunderedThousandModifier)) ? 0
+        : round(($summedCount / $this->stateLagDays)/$perHunderedThousandModifier, 2);
     }
 }
